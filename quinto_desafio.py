be802385d822f5ef9e8f0bf0db32260982d5f1e5
Antonella Denisse Cuenca <denisseanto11@hotmail.com>
2022-03-31 09:57:57 -0300

    Simplificacion del codigo de la funcion

--- quinto_desafio.py
+++ quinto_desafio.py
@@ -2,11 +2,7 @@ def encripto(cadena, n=1):
     """ Esta funcion retorna a partir de un string, su equivalente
         en cifrado cesar, segun su variacion de desplazamiento expresado en n
     """
-    c = []
-    for ch in cadena:
-        c.append(ch)
-    c = list(map(lambda x: ord(x) + n, c))
-    c = list(map(lambda x: chr(x), c))
+    c = list(map(lambda x: chr(ord(x) + n), cadena))
     c = ''.join(c)
     return c
 
